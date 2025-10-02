fb137afe672affac4a2ff6a340d8dcce0ed138ac
Kenzo Trindade <kenzo.trindade@gmail.com>
2025-10-02 11:35:11 +0200

    HelloWorld

---
+++ main.py
@@ -0,0 +1,13 @@
+def exercice1():
+    print("Exercice 1 : Bonjour le monde !")
+    print("Hello World !")
+
+def main():
+    choix = input("Entrez le numéro de l'exercice à exécuter : ")
+    if choix == "1":
+        exercice1()
+    else:
+        print("Exercice non reconnu.")
+
+if __name__ == "__main__":  
+    main()
