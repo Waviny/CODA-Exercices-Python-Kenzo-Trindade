44260df28f0b741095345c8a13a328bd1f49d73e
Kenzo Trindade <kenzo.trindade@gmail.com>
2025-10-02 11:44:26 +0200

    PatchFonctions - Exercice 4

--- main.py
+++ main.py
@@ -6,17 +6,27 @@ def main():
     choix = input("Entrez le numéro de l'exercice à exécuter : ")
     if choix == "1":
         exercice1()
+    elif choix =="2":
+        exercice2()
+    elif choix =="3":
+        exercice3()
+    elif choix == "4":
+        exercice4()
     else:
         print("Exercice non reconnu.")
 
 if __name__ == "__main__":  
     main()
 
-# Niveau Découverte 1
-
-print("Bonjour le monde !")
-
 # Niveau Découverte 2
+def exercice2():
+    print("Bonjour le monde !")
 
-name = input("Quel est ton prénom")
-print("Bonjour", name)
+# Niveau Découverte 3
+def exercice3():
+    name = input("Quel est ton prénom")
+    print("Bonjour", name)
+
+# Niveau Découverte 4
+def exercice4():
+    print("Première ligne \n Deuxième ligne \n Troisème ligne")
