ebff55fa00ae6f78a0e5f331a77bb3d177d9f10b
Kenzo Trindade <kenzo.trindade@gmail.com>
2025-10-02 11:50:49 +0200

    Exercice5

--- main.py
+++ main.py
@@ -2,22 +2,6 @@ def exercice1():
     print("Exercice 1 : Bonjour le monde !")
     print("Hello World !")
 
-def main():
-    choix = input("Entrez le numéro de l'exercice à exécuter : ")
-    if choix == "1":
-        exercice1()
-    elif choix =="2":
-        exercice2()
-    elif choix =="3":
-        exercice3()
-    elif choix == "4":
-        exercice4()
-    else:
-        print("Exercice non reconnu.")
-
-if __name__ == "__main__":  
-    main()
-
 # Niveau Découverte 2
 def exercice2():
     print("Bonjour le monde !")
@@ -30,3 +14,27 @@ def exercice3():
 # Niveau Découverte 4
 def exercice4():
     print("Première ligne \n Deuxième ligne \n Troisème ligne")
+
+# Niveau Découverte 5
+def exercice5():
+    annee = input("Quelle est votre année de naissance : ")
+    age = 2025 - annee
+    print(age)
+
+def main():
+    choix = input("Entrez le numéro de l'exercice à exécuter : ")
+    if choix == "1":
+        exercice1()
+    elif choix =="2":
+        exercice2()
+    elif choix =="3":
+        exercice3()
+    elif choix == "4":
+        exercice4()
+    elif choix == "5":
+        exercice5()
+    else:
+        print("Exercice non reconnu.")
+
+if __name__ == "__main__":  
+    main()
